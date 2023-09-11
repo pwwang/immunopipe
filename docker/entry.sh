6e92aa66e4333d2eabb6cd76c348565b0a0f4636
pwwang <pwwang@pwwang.com>
2023-09-11 14:50:31 -0500

    🎨 Update docker entry point

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,16 +4,34 @@
 if [ $# -eq 0 ]; then
     echo "There are two ways to run the pipeline:"
     echo "1. Run the pipeline from the command line"
-    echo "   docker run <options> justold/immunopipe:master @<configfile> [options]"
-    echo "   singularity run <options> docker://justold/immunopipe:master @<configfile> [options]"
-    echo "2. Run the pipeline using pipen-board:"
-    echo "   docker run <options> justold/immunopipe:master board immunopipe:Immunopipe -a /immunopipe/board.toml [options]"
-    echo "   singularity run <options> docker://biopipen/scrna-basic:master board immunopipe:Immunopipe -a /workdir/board.toml [options]"
+    echo "   $ docker run <options> justold/immunopipe:<tag> \\"
+    echo "       @<configfile> [options]"
+    echo "   $ singularity run <options> docker://justold/immunopipe:<tag> \\"
+    echo "       @<configfile> [options]"
+    echo "   Or:"
+    echo "   $ docker run <options> justold/immunopipe:<tag> \\"
+    echo "       immunopipe @<configfile> [options]"
+    echo "   $ singularity run <options> docker://justold/immunopipe:<tag> \\"
+    echo "       immunopipe @<configfile> [options]"
+    echo ""
+    echo "2. Run the pipeline using \`pipen-board\`:"
+    echo "   $ docker run <options> justold/immunopipe:<tag> \\"
+    echo "       board immunopipe:Immunopipe -a /immunopipe/board.toml [options]"
+    echo "   $ singularity run <options> docker://biopipen/scrna-basic:<tag> \\"
+    echo "       board immunopipe:Immunopipe -a /workdir/board.toml [options]"
+    echo "   Or:"
+    echo "   $ docker run <options> justold/immunopipe:<tag> \\"
+    echo "       pipen board immunopipe:Immunopipe -a /immunopipe/board.toml [options]"
+    echo "   $ singularity run <options> docker://biopipen/scrna-basic:<tag> \\"
+    echo "       pipen board immunopipe:Immunopipe -a /workdir/board.toml [options]"
     exit 1
-elif [ "$1" == "board" ]; then
+elif [[ "$1" == "board" ]]; then
     # Run the pipeline using pipen-board
     pipen $@
+elif [[ "$1" == "@"* ]]; then
+    # Run the pipeline using immunopipe
+    immunopipe $@
 else
     # Run the pipeline from the command line
-    immunopipe $@
+    eval "$@"
 fi
