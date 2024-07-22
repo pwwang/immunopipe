70a00c5e76435172b02396e0093aa764e6256f1e
pwwang <1188067+pwwang@users.noreply.github.com>
2024-07-22 09:19:41 -0700

    Delete 1.3.5 directory

--- 1.3.5/assets/script.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    // $('.rst-content a[href^="http"]').attr('target', '_blank');
-
-    if (window.location.hash !== '') {
-        $(`.rst-content #faq ~ details${window.location.hash}`).attr('open', true);
-    }
-})(jQuery);
