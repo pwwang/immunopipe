2c51b6121b031f6c4ceb06156aa9e301243a177d
pwwang <pwwang@pwwang.com>
2025-03-20 16:45:32 -0500

    test: update assertions in sampleinfo and seuratpreparing tests

--- tests/test_sampleinfo.py
+++ tests/test_sampleinfo.py
@@ -7,19 +7,7 @@ pytest_order = 1
 
 @pytest.mark.forked
 def test_sampleinfo(tmp_path, request):
-
     outdir = run_process(
         "SampleInfo", "sampleinfo.config.toml", tmp_path, request=request
     )
-    assert outdir.joinpath("Age_distribution (boxplot).png").is_file()
-    assert outdir.joinpath(
-        "Age_distribution_per_Diagnosis (violin + boxplot).png"
-    ).is_file()
-    assert outdir.joinpath(
-        "Age_distribution_per_Diagnosis (violin + boxplot).png"
-    ).is_file()
-    assert outdir.joinpath(
-        "Age_distribution_per_Sex_in_each_Diagnosis (boxplot).png"
-    ).is_file()
-    assert outdir.joinpath("N_Samples_per_Diagnosis (bar).png").is_file()
-    assert outdir.joinpath("N_Samples_per_Diagnosis (pie).png").is_file()
+    assert outdir.joinpath("Age_distribution-Histogram-.png").is_file()
--- tests/test_seuratpreparing.py
+++ tests/test_seuratpreparing.py
@@ -15,20 +15,6 @@ def test_seuratpreparing(tmp_path, request):
         request=request,
     )
     assert outdir.joinpath("sampleinfo.seurat.RDS").is_file()
-    assert outdir.joinpath("DoubletFinder_doublets_singlets.txt").is_file()
-    assert outdir.joinpath("DoubletFinder_summary.txt").is_file()
-    assert outdir.joinpath("plots", "dim.txt").is_file()
-    assert outdir.joinpath("plots", "nCount_RNA.vln.png").is_file()
-    assert outdir.joinpath("plots", "nFeature_RNA-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "nFeature_RNA.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-hb-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-hb.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-mt-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-mt.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-plat-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-plat.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-ribo-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-ribo.vln.png").is_file()
 
 
 @pytest.mark.forked
@@ -42,15 +28,3 @@ def test_seuratpreparing_sct(tmp_path, request):
         request=request,
     )
     assert outdir.joinpath("sampleinfo.seurat.RDS").is_file()
-    assert outdir.joinpath("plots", "dim.txt").is_file()
-    assert outdir.joinpath("plots", "nCount_RNA.vln.png").is_file()
-    assert outdir.joinpath("plots", "nFeature_RNA-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "nFeature_RNA.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-hb-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-hb.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-mt-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-mt.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-plat-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-plat.vln.png").is_file()
-    assert outdir.joinpath("plots", "percent-ribo-nCount_RNA.scatter.png").is_file()
-    assert outdir.joinpath("plots", "percent-ribo.vln.png").is_file()
