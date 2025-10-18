6a095bfbf0c71eed93e1f7e0b3230f6c84673233
pwwang <pwwang@pwwang.com>
2025-10-18 01:20:24 -0700

    fix: update loading pipeline initialization to include help options
    - Modified `just_loading` to include help flags in `is_loading_pipeline()`

--- immunopipe/processes.py
+++ immunopipe/processes.py
@@ -45,7 +45,7 @@ from .inhouse import (
 from .validate_config import validate_config
 
 toml_dumps = FILTERS["toml_dumps"]
-just_loading = is_loading_pipeline()
+just_loading = is_loading_pipeline("-h", "--help", "-h+", "--help+")
 config = validate_config()
 
 # https://pwwang.github.io/immunopipe/latest/
